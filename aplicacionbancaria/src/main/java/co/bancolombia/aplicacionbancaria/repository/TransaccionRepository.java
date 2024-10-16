93a43da8cedc44d08c9e834c1aab70f87d242ea5
Alexis Garcia <aldgarci@bancolombia.com.co>
2024-10-15 22:36:20 -0500

    Taller 4 - aldgarci

---
+++ aplicacionbancaria/src/main/java/co/bancolombia/aplicacionbancaria/repository/TransaccionRepository.java
@@ -0,0 +1,7 @@
+package co.bancolombia.aplicacionbancaria.repository;
+
+import co.bancolombia.aplicacionbancaria.model.Transaccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransaccionRepository extends JpaRepository <Transaccion, Long>{
+}
